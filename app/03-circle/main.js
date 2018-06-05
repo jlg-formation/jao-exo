ec1268332a8b4c074e8a6d3d85eede66d636dc81
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-06-05 10:16:23 +0200

    ok

--- app/03-circle/main.js
+++ app/03-circle/main.js
@@ -16,18 +16,32 @@ class Circle {
     }
 
     setPoint(angle) {
-        const a = angle * 2 * Math.PI / 360;
+        const a = - angle * 2 * Math.PI / 360;
         const svgns = "http://www.w3.org/2000/svg";
         const svg = document.querySelector('svg');
         const point = document.createElementNS(svgns, 'circle');
         point.setAttribute('cx', this.cx + this.radius * Math.cos(a));
-        point.setAttribute('cy', this.cy - this.radius * Math.sin(a));
+        point.setAttribute('cy', this.cy + this.radius * Math.sin(a));
         point.setAttribute('r', 3);
         point.setAttribute('fill', '#000');
         point.setAttribute('stroke', '#000');
         point.setAttribute('stroke-width', '2');
 
         svg.appendChild(point);
+    }
+
+    setLine(angle1, angle2) {
+        const svgns = "http://www.w3.org/2000/svg";
+        const svg = document.querySelector('svg');
+        const line = document.createElementNS(svgns, 'line');
+        line.setAttribute('x1', 10);
+        line.setAttribute('y1', 20);
+        line.setAttribute('x2', 50);
+        line.setAttribute('y2', 100);
+        line.setAttribute('stroke', '#000');
+        line.setAttribute('stroke-width', '2');
+
+        svg.appendChild(line);
     }
 }
 
@@ -37,6 +51,8 @@ function main() {
     c.setPoint(30);
     c.setPoint(60);
     c.setPoint(90);
+    c.setPoint(270);
+    c.setLine(30, 90);
 }
 
 main();
