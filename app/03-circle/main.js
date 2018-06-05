f99621bb67d24e9ddfadfef8604afa00db21d160
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-06-05 09:50:36 +0200

    ok

--- app/03-circle/main.js
+++ app/03-circle/main.js
@@ -1,19 +1,37 @@
 class Circle {
-    constructor() { }
+    constructor(radius) {
+        this.radius = radius;
+    }
 
     draw() {
         const div = document.querySelector('div');
+        const r = this.radius;
         div.innerHTML = `
-<svg width="600" height="600">
-    <circle cx="300" cy="300" r="200" stroke="#000" stroke-width="2" fill="transparent" />
+<svg width="${3 * r}" height="${3 * r}">
+    <circle cx="${1.5 * r}" cy="${1.5 * r}" r="${r}" stroke="#000" stroke-width="2" fill="transparent" />
 </svg>
     `;
+    }
+
+    setPoint(angle) {
+        const svgns = "http://www.w3.org/2000/svg";
+        const svg = document.querySelector('svg');
+        const point = document.createElementNS(svgns, 'circle');
+        point.setAttributeNS(null, 'cx', 1.5 * this.radius);
+        point.setAttributeNS(null, 'cy', 1.5 * this.radius);
+        point.setAttributeNS(null, 'r', 10);
+        point.setAttributeNS(null, 'fill', '#000');
+        point.setAttributeNS(null, 'stroke', '#000');
+        point.setAttributeNS(null, 'stroke-width', '2');
+
+        svg.appendChild(point);
     }
 }
 
 function main() {
-    const c = new Circle();
+    const c = new Circle(200);
     c.draw();
+    c.setPoint(10);
 }
 
 main();
