bfe0143c826c3f77403c4a8ae905420a7c00d342
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-06-05 09:29:58 +0200

    ok

--- app/03-circle/main.js
+++ app/03-circle/main.js
@@ -1,15 +1,19 @@
-function makeCircle() {
-    const div = document.querySelector('div');
-    div.innerHTML = `
+class Circle {
+    constructor() { }
+
+    draw() {
+        const div = document.querySelector('div');
+        div.innerHTML = `
 <svg width="600" height="600">
     <circle cx="300" cy="300" r="200" stroke="#000" stroke-width="2" fill="transparent" />
 </svg>
     `;
-
+    }
 }
 
 function main() {
-    makeCircle();
+    const c = new Circle();
+    c.draw();
 }
 
 main();
